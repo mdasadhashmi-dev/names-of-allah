10f25ea7f75fb77b0f0cc227bc08a43de10204a8
mdasadhashmi-dev <mdasadhashmi@gmail.com>
2025-12-08 17:51:02 +0400

    Initial Commit

---
+++ Names of Allah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Names of Allah
+//
+//  Created by Mohammed Asad Hashmi on 08/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Names of Allah/Names_of_AllahApp.swift
@@ -0,0 +1,17 @@
+//
+//  Names_of_AllahApp.swift
+//  Names of Allah
+//
+//  Created by Mohammed Asad Hashmi on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Names_of_AllahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
